c6f563bc56638650db6c0798ee79702235afda9d
M Towseef Hossain <towseef.22aug@gmail.com>
2019-05-28 02:28:52 -0400

    Add files via upload

--- FIFA.py
+++ FIFA.py
@@ -3,53 +3,42 @@ import pandas as pd
 from re import sub
 from decimal import Decimal
 
-data = pd.read_csv('data.csv')
+data = pd.read_csv('data.csv') #reading the CSV Data File
 
-print(list(data.columns.values))
+print(list(data.columns.values)) #Producing the list of variables the user can choose 
 
-x = str(input("Select the x-axis variable "))
-y = str(input("Select the y-axis variable "))
+x = str(input("Select the x-axis variable ")) #Choosing the firse variable
+type = str(input("Type of graph? Scatterplot(Type S) Pie Chart (Type P) Distribution Histogram (Type H) Bar Chart of Top 10 (Type B) ")) #Type of data visualization
 
-type = str(input("Type of graph? Scatterplot(Type S) "))
+if type == 'P': #code to output Pie Charts
+	plt.pie(data[x].value_counts(), labels=data[x].value_counts().index)
+	plt.show()
 
-remove_nan = data.dropna(subset=[x, y])
-
-def value_to_float(x):
-    if 'K' in x:
-        if len(x) > 1:
-            return (float(x.replace('€', '').replace('K', ''))) * 1000
-        return 1000.0
-    if 'M' in x:
-        if len(x) > 1:
-            return (float(x.replace('€', '').replace('M', ''))) * 1000000
-        return 1000000.0
-    if 'B' in x:
-        return (float(x.replace('€', '').replace('B', ''))) * 1000000000
-    return 0.0
+if type == 'H': #code to output Histograms
+	plt.hist(data[x])
+	plt.show()
+	print("The mean %s of players in FIFA 19 is %s" % x, data[x].mean())
+	print("The median %s of players in FIFA 19 is %s" % x, data[x].median())
+	print("Most players in FIFA 19 have a %s of %s" % x, data[x].mode()[0])
 
 
-if remove_nan[x].dtype == 'O':
-	clean_data_x = remove_nan[x].map(value_to_float, axis=1)
-else:
-	clean_data_x = remove_nan[x]
-
-if remove_nan[x].dtype == 'O':
-	clean_data_y = remove_nan[y].map(value_to_float, axis=1)
-else:
-	clean_data_y = remove_nan[y]
-
-if type == 'S':
-	plt.scatter(clean_data_x, clean_data_y, c="r")
+if type == 'S': #code to output Scatterplots
+	y = str(input("Select the y-axis variable ")) #Asking user for y-axis variable for the scatterplot
+	plt.scatter(data[x], data[y], c="r")
 	plt.title("%s against %s" % (x,y))
 	plt.xlabel(x)
 	plt.ylabel(y)
 	plt.gca().invert_yaxis()
 	plt.show()
-	corr_cof = clean_data_x.corr(clean_data_y)
+	corr_cof = x.corr(y)
 	if (abs(corr_cof) > 0.7):
 		print("There is a strong correlation between %s and %s" % (x, y))
 	else:
 		print("There is a weak correlation between %s and %s" % (x, y))
+
+if type == 'B': #code to output BarCharts
+	plt.bar(data[x].value_counts().head(10).index, data[x].value_counts().head(10).values)
+	plt.show()
 
 input()
 
